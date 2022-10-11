063844008e6186f06bb6091061ce7cdaf74865f4
William Khan <wnkhan@crimson.ua.edu>
2022-10-10 21:06:59 -0400

    Separated income in categories, and modified categories to pertain to a given month.

--- excel/workbook_builder.py
+++ excel/workbook_builder.py
@@ -19,22 +19,29 @@ class WorkbookBuilder:
 
         return sheetnames
 
-    def get_categories(self) -> List[str]:
-        return set([trans.category for trans in self.transaction_list])
+    def get_categories_by_month(self, month_and_year : str) -> List[str]:
+        monthly_transactions = self.monthly_transactions[month_and_year]
+        return set([trans.category for trans in monthly_transactions])
 
     def update_workbook_data(self) -> None:
         for sheet in self.get_sheetnames():
+
             if self.monthly_transactions.get(sheet) is None:
                 self.monthly_transactions[sheet] = [] 
+
             if self.monthly_transactions_by_category.get(sheet) is None:
                 self.monthly_transactions_by_category[sheet] = {}
-                for category in self.get_categories():
-                    self.monthly_transactions_by_category[sheet][category] = 0.0
+
         for trans in self.transaction_list:
             month_and_year = get_month_and_year(trans.t_date)
             if self.monthly_transactions.get(month_and_year) is not None:
                 self.monthly_transactions[month_and_year].append(trans)
-            self.monthly_transactions_by_category[month_and_year][trans.category] += float(trans.amount)
+        
+        for month in self.get_sheetnames():
+            for category in self.get_categories_by_month(month):
+                self.monthly_transactions_by_category[month][category] = 0.0
+            for trans in self.monthly_transactions[month]:
+                self.monthly_transactions_by_category[month][trans.category] += float(trans.amount)
 
             
 
@@ -53,19 +60,26 @@ class WorkbookBuilder:
                 trans_details = [trans.t_date,trans.d1,trans.category,float(trans.amount),trans.status]
                 wb[sheet].append(trans_details)
 
-            for category in self.get_categories():
+            income_category = []
+            for category in self.get_categories_by_month(sheet):
                 category_amount = self.monthly_transactions_by_category[sheet][category]
                 category_row = [category,category_amount]
+
                 if category_amount < 0:
                     wb[sheet+"-categories"].append(category_row)
+                else:
+                    income_category.append(category_row)
+
+            wb[sheet+"-categories"].append([10*"-",10*"-"])
+            [wb[sheet+"-categories"].append(row) for row in income_category]
 
             pie = PieChart()
-            labels = Reference(wb[sheet+"-categories"],min_col=1,min_row=1,max_row=len(self.get_categories()))
-            data = Reference(wb[sheet+"-categories"],min_col=2,min_row=1,max_row=len(self.get_categories()))
+            labels = Reference(wb[sheet+"-categories"],min_col=1,min_row=1,max_row=len(self.get_categories_by_month(sheet))-len(income_category))
+            data = Reference(wb[sheet+"-categories"],min_col=2,min_row=1,max_row=len(self.get_categories_by_month(sheet))-len(income_category))
             pie.add_data(data,titles_from_data=False)
             pie.set_categories(labels)
             pie.title = "Spending by Category"
-            wb[sheet+"-categories"].add_chart(pie,"G1")
+            wb[sheet+"-categories"].add_chart(pie,"D1")
 
         wb.save('bk_download.xlsx')
 
