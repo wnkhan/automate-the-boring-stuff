dd0fe053ed19c6de830eab375477e032fcbe8b92
William Khan <wnkhan@crimson.ua.edu>
2022-11-06 23:17:18 -0500

    Updated script for combining csv's to use pandas.

--- cli_tools/update_transactions.py
+++ cli_tools/update_transactions.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from os import DirEntry
 from typing import List
 
@@ -6,30 +7,27 @@ def get_bank_data_files() -> List[str]:
     bank_data_files = []
 
     if(user_home := os.environ.get('USERPROFILE')):
+        with os.scandir(user_home +'\Downloads') as directory:
+            csv_s = filter(is_csv,directory)
 
-        print("User home: " + user_home)
-        downloads_path = user_home + '\\Downloads'
+            bank_data_df_s = []
+            for csv in csv_s:
+                df = pd.read_csv(csv)
+                bank_data_df_s.append(df)
+            master_df = pd.concat(bank_data_df_s, ignore_index=True)
 
-        with os.scandir(downloads_path) as directory:
+            category_pending_df = master_df.loc[master_df['Category'] == 'Category Pending']
 
-                csv_s = filter(is_csv,directory)
-                csv_lines = set()
+            update_dict = {}
+            for row in category_pending_df.itertuples():
+                search_df = master_df.loc[(master_df['Date'] == row[1]) & (master_df['Description'] == row[2])]
+                update_dict.setdefault(row[0],search_df.iloc[0]['Category'])
 
-                for csv in csv_s:
-                    with open(csv) as csv_handle:
-                        for line in csv_handle:
-                            csv_lines.add(line)
+            for key, value in update_dict.items():
+                master_df.at[key,'Category'] = value
 
-                csv_lines = list(csv_lines)
-                csv_lines.sort(reverse=True)
-
-                with open(user_home + '\\Repos\\automate-the-boring-stuff\\excel\\bk_download.csv','w') as master_csv:
-                    for line in csv_lines:
-                        master_csv.write(line)
-                        
-
-        
-
+            master_df.drop_duplicates()
+            master_df.to_csv(os.path.join(user_home,'Repos','automate-the-boring-stuff','excel','bk_download.csv'), index=False)
     else:
         print("Couldn't find user home.")
 
