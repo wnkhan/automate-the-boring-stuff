841d317780f5e2a63ba890d8afa4f78bfa6c086f
William Khan <william@d3b.center>
2023-02-13 10:51:29 -0500

    Removed unused.

--- expense_analysis/main.py
+++ expense_analysis/main.py
@@ -1,10 +1,8 @@
 from trans_db_api import TransactionDatabase
-from extractor import TransactionExtractor
 from workbook_builder import WorkbookBuilder
 
 def main():
     transaction_db = TransactionDatabase('transaction.sqlite')
-
     workbook_builder = WorkbookBuilder(transaction_db)
     workbook_builder.build_workbook()
 
