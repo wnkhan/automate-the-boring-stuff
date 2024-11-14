b8645782d82045ac0c3c9f874aeac634ac4f8376
William Khan <wnkhan32@gmail.com>
2024-11-14 18:11:04 -0500

    Updated main with new etl.

--- expense_analysis/main.py
+++ expense_analysis/main.py
@@ -1,9 +1,16 @@
-from trans_db_api import TransactionDatabase
+import os
 from workbook_builder import WorkbookBuilder
+from etl import Transaction_Etl
+from trans_db_api import TransactionDatabase
 
 def main():
-    transaction_db = TransactionDatabase('transaction.sqlite')
-    workbook_builder = WorkbookBuilder(transaction_db)
+    etl = Transaction_Etl()
+    etl.ingest('bk_download','csv','~/Downloads/mounted')
+    etl.transform()
+    etl.load('data')
+
+    db_path = os.path.join(os.getcwd(),"data.db")
+    workbook_builder = WorkbookBuilder(TransactionDatabase(db_path))
     workbook_builder.build_workbook()
 
 if __name__ == '__main__':
