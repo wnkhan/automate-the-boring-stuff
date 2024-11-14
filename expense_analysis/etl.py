446155e31ff7100d2d4e478a99094f3e1274153f
William Khan <wnkhan32@gmail.com>
2024-11-14 18:00:31 -0500

    Added new ETL.

---
+++ expense_analysis/etl.py
@@ -0,0 +1,43 @@
+import os
+import pandas as pd
+import sqlite3
+from field_masks import category_mappings, description_mappings
+
+class Transaction_Etl:
+
+    def __init__(self):
+        self.df = None
+
+    def ingest(self, table_name, extension, path="~/Downloads"):
+        match extension:
+            case "csv":
+                self.df = pd.read_csv(os.path.join(path,".".join([table_name,extension])))
+            case "tsv":
+                self.df = pd.read_table(os.path.join(path,".".join(table_name,extension)))
+
+
+    def transform(self):
+        for category, mapping_list in category_mappings.items():
+            self.df.loc[self.df['Category'].isin(mapping_list),'Category'] = category
+
+        for category, mapping_list in description_mappings.items():
+            self.df.loc[self.df['Description'].isin(mapping_list), 'Category'] = category
+
+        self.df['id'] = range(1, len(self.df) + 1)
+        self.df['month'] = self.df['Date'].apply(lambda date_str: date_str.split('-')[1])
+        self.df['year'] = self.df['Date'].apply(lambda date_str: date_str.split('-')[0])
+        self.df['day'] = self.df['Date'].apply(lambda date_str: date_str.split('-')[2])
+        self.df.rename(columns={'Description':'d1', 
+                                'Original Description': 'd2', 
+                                'Category': 'category',
+                                'Amount':'amount',
+                                'Status':'status'},
+                        inplace=True)
+
+        column_ordering = ['id','day','month','year','d1','d2','category','amount','status']
+        self.df = self.df[column_ordering]
+
+    def load(self, db_name):
+        with sqlite3.connect(".".join([db_name,"db"])) as conn:
+            self.df.to_sql('transactions',conn,if_exists='replace',index=False)
+        
---
+++ expense_analysis/field_masks.py
@@ -0,0 +1,22 @@
+
+category_mappings = {
+    'Eating Out': ["Coffee Shops","Restaurants", 
+                   "Food & Dining", "Fast Food",
+                   "Food Dining","Alcohol & Bars"],
+    'Bill & Utilities': ["Mortgage & Rent",
+                                   "Car",
+                                   "Mobile Phone", 
+                                   "Rent", "Storage",
+                                   "Utilities","Internet"],
+    'Shopping': ['Electronics & Software','Pet Food & Supplies','Clothing']
+}
+
+description_mappings = {
+    'Bill & Utilities': ["Fi","Auto Ivr"],
+    'Subscription': ["Apple","Netflix",
+                     "Spotify","ExpressVPN",
+                     "Youtube","Medium",
+                    "LinkedIn","Pluralsight",
+                    "Game Pass","Prime","Amznfreetime",
+                    "Zoom","Blink"]
+}
