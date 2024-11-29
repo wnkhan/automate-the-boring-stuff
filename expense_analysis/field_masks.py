64f529c66dd90895421d4b53fcde2e8738e00512
William <wnkhan@crimson.ua.edu>
2024-11-29 16:36:05 -0500

    Updated category and description mappings.

--- expense_analysis/field_masks.py
+++ expense_analysis/field_masks.py
@@ -1,22 +1,39 @@
 
 category_mappings = {
-    'Eating Out': ["Coffee Shops","Restaurants", 
-                   "Food & Dining", "Fast Food",
-                   "Food Dining","Alcohol & Bars"],
-    'Bill & Utilities': ["Mortgage & Rent",
-                                   "Car",
-                                   "Mobile Phone", 
-                                   "Rent", "Storage",
-                                   "Utilities","Internet"],
-    'Shopping': ['Electronics & Software','Pet Food & Supplies','Clothing']
+    'Eating Out': ["Coffee Shops",
+                   "Restaurants", 
+                   "Food & Dining", 
+                   "Fast Food",
+                   "Food Dining",
+                   "Alcohol & Bars"],
+
+    'Bills & Utilities': ["Mortgage & Rent",
+                          "Car",
+                          "Mobile Phone", 
+                          "Rent", "Storage",
+                          "Utilities",
+                          "Internet"],
+
+    'Shopping': ['Electronics & Software',
+                 'Pet Food & Supplies',
+                 'Clothing']
 }
 
 description_mappings = {
-    'Bill & Utilities': ["Fi","Auto Ivr"],
-    'Subscription': ["Apple","Netflix",
-                     "Spotify","ExpressVPN",
-                     "Youtube","Medium",
-                    "LinkedIn","Pluralsight",
-                    "Game Pass","Prime","Amznfreetime",
-                    "Zoom","Blink"]
+    'Bills & Utilities': ["Fi",
+                          "Auto Ivr"],
+    'Subscription': ["Apple",
+                     "Netflix",
+                     "Spotify",
+                     "ExpressVPN",
+                     "Youtube",
+                     "Medium",
+                    "LinkedIn",
+                    "Pluralsight",
+                    "Game Pass",
+                    "Prime",
+                    "Amznfreetime",
+                    "Zoom",
+                    "Blink",
+                    "Leetcode.com"]
 }
