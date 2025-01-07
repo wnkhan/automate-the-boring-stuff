306646f27220c672ee5d3fa7fc522b793bf5bdc0
William Khan <wnkhan32@gmail.com>
2025-01-06 20:48:08 -0500

    Add refresh_project_data function to clean up project files before ingestion

--- expense_analysis/main.py
+++ expense_analysis/main.py
@@ -1,9 +1,21 @@
 import os
 from workbook_builder import WorkbookBuilder
-from etl import Transaction_Etl
+from etl import Transaction_Etl, get_user_home
 from trans_db_api import TransactionDatabase
 
+def refresh_project_data():
+    for file in ['data.db','bk_download.xlsx']:
+        project_path = os.path.join(os.getcwd(),file)
+        expense_analysis_path = os.path.join(os.getcwd(), 'expense_analysis',file)
+
+        if os.path.exists(project_path):
+            os.remove(project_path)
+        if os.path.exists(expense_analysis_path):
+            os.remove(expense_analysis_path)
+
 def main():
+    refresh_project_data()
+
     etl = Transaction_Etl()
     etl.ingest('bk_download','csv')
     etl.transform()
